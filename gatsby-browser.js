85064714ecda401d6f6d0bd120741a5e4ca67ed4
Mahadev Mandal <mandalmahadev5@gmail.com>
2024-03-25 13:30:30 +0545

    test2

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,18 @@
 
 
+
+import React from "react";
+
+export const wrapRootElement = ({ element, props }) => {
+
+  document.addEventListener('DOMContentLoaded', (e) => {
+    const formContainer = document.querySelector('#form-container');
+    const formStackForm = document.querySelector('#fsform-container');
+    console.log(formContainer, formStackForm)
+    if (formContainer && formStackForm) {
+      formContainer.appendChild(formStackForm);
+    }
+  })
+
+  return element
+}
