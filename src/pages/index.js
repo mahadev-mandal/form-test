6ff4cc34e7e7a151086471c38e5f529aecd7553d
Mahadev Mandal <mandalmahadev5@gmail.com>
2024-03-25 13:34:46 +0545

    test 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../components/dfa-theme/layout";
 import "./index.scss";
 // import { Col, Row } from "react-flexbox-grid";
 
 const McqPage = () => {
   console.log('deploy test outside')
-
+  useEffect(() => {
+    const formContainer = document.querySelector('#form-container');
+    const formStackForm = document.querySelector('#fsform-container');
+    console.log(formContainer, formStackForm)
+    if (formContainer && formStackForm) {
+      formContainer.appendChild(formStackForm);
+    }
+  }, [])
   return (
     <Layout>
       <section id="form-container">
-        
+
       </section>
     </Layout>
   );
